6d03b4529894ab1f9b9ce9bee1df63ec02e686c5
Mykola Sakhno <sakhno.m@gmail.com>
2025-04-28 04:59:03 +0300

    add test for 100 votes for task4

--- problem4/public4.spec.ts
+++ problem4/public4.spec.ts
@@ -1,5 +1,5 @@
 import '@ton/test-utils';
-import { Blockchain, SandboxContract, TreasuryContract  } from '@ton/sandbox';
+import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
 import { toNano } from '@ton/core';
 import { Proposal } from '../output/solution4_Proposal';
 import { ProposalMaster } from '../output/solution4_ProposalMaster';
@@ -11,7 +11,7 @@ describe("solution4", () => {
     let masterDeployer: SandboxContract<TreasuryContract>;
     let proposalMaster: SandboxContract<ProposalMaster>;
 
-    beforeEach(async () =>  {
+    beforeEach(async () => {
         blockchain = await Blockchain.create();
         masterDeployer = await blockchain.treasury('deployer');
 
@@ -67,4 +67,48 @@ describe("solution4", () => {
         expect(await proposal.getProposalState()).toMatchObject({ yesCount: 1n, noCount: 0n });
     });
 
+    it('100 votes', async () => {
+        const numberOfVoters = 100;
+        const expectedYesCount = 50;
+
+        // create proposal
+        const currentTime = BigInt(Math.floor(Date.now() / 1000));
+        await proposalMaster.send(
+            masterDeployer.getSender(),
+            {
+                value: toNano('0.1'),
+                bounce: false,
+            },
+            {
+                $$type: 'DeployNewProposal',
+                votingEndingAt: currentTime + 24n * 60n * 60n,
+            },
+        );
+
+        const proposal = blockchain.openContract(
+            await Proposal.fromInit({
+                $$type: 'ProposalInit',
+                master: proposalMaster.address,
+                proposalId: 0n,
+            }),
+        );
+
+        for (let iVoter = 0; iVoter < numberOfVoters; iVoter++) {
+            // vote
+            const voter = await blockchain.treasury('voter'+iVoter);
+            const voteValue = iVoter < expectedYesCount;
+
+            await proposal.send(
+                voter.getSender(),
+                { value: toNano('0.1') },
+                {
+                    $$type: 'Vote',
+                    value: voteValue,
+                },
+            );
+        }
+
+        // the vote was counted
+        expect(await proposal.getProposalState()).toMatchObject({ yesCount: BigInt(expectedYesCount), noCount: BigInt(numberOfVoters - expectedYesCount) });
+    });
 });
