ecfeabff710a066108042d692b5b78553d7b2e0f
Mykola Sakhno <sakhno.m@gmail.com>
2025-04-28 04:48:56 +0300

    refactor tests for task4

--- problem4/public4.spec.ts
+++ problem4/public4.spec.ts
@@ -1,59 +1,70 @@
 import '@ton/test-utils';
-import { Blockchain } from '@ton/sandbox';
+import { Blockchain, SandboxContract, TreasuryContract  } from '@ton/sandbox';
 import { toNano } from '@ton/core';
 import { Proposal } from '../output/solution4_Proposal';
 import { ProposalMaster } from '../output/solution4_ProposalMaster';
+import { before } from 'node:test';
 
-it('solution4', async () => {
-    const blockchain = await Blockchain.create();
 
-    // init master contract
-    const proposalMaster = blockchain.openContract(
-        await ProposalMaster.fromInit(),
-    );
+describe("solution4", () => {
+    let blockchain: Blockchain;
+    let masterDeployer: SandboxContract<TreasuryContract>;
+    let proposalMaster: SandboxContract<ProposalMaster>;
 
-    // deploy master contract
-    const masterDeployer = await blockchain.treasury('deployer');
-    await proposalMaster.send(
-        masterDeployer.getSender(),
-        {
-            value: toNano('0.01'),
-        },
-        null, // empty message, handled by `receive()` without parameters
-    );
+    beforeEach(async () =>  {
+        blockchain = await Blockchain.create();
+        masterDeployer = await blockchain.treasury('deployer');
 
-    // create proposal
-    const currentTime = BigInt(Math.floor(Date.now() / 1000));
-    await proposalMaster.send(
-        masterDeployer.getSender(),
-        {
-            value: toNano('0.1'),
-            bounce: false,
-        },
-        {
-            $$type: 'DeployNewProposal',
-            votingEndingAt: currentTime + 24n * 60n * 60n,
-        },
-    );
+        // init master contract
+        proposalMaster = blockchain.openContract(
+            await ProposalMaster.fromInit(),
+        );
+        await proposalMaster.send(
+            masterDeployer.getSender(),
+            {
+                value: toNano('0.01'),
+            },
+            null, // empty message, handled by `receive()` without parameters
+        );
 
-    // vote
-    const voter = await blockchain.treasury('voter');
-    const proposal = blockchain.openContract(
-        await Proposal.fromInit({
-            $$type: 'ProposalInit',
-            master: proposalMaster.address,
-            proposalId: 0n,
-        }),
-    );
-    await proposal.send(
-        voter.getSender(),
-        { value: toNano('0.1') },
-        {
-            $$type: 'Vote',
-            value: true,
-        },
-    );
+    });
 
-    // the vote was counted
-    expect(await proposal.getProposalState()).toMatchObject({ yesCount: 1n, noCount: 0n });
+
+    it('basic test', async () => {
+        // create proposal
+        const currentTime = BigInt(Math.floor(Date.now() / 1000));
+        await proposalMaster.send(
+            masterDeployer.getSender(),
+            {
+                value: toNano('0.1'),
+                bounce: false,
+            },
+            {
+                $$type: 'DeployNewProposal',
+                votingEndingAt: currentTime + 24n * 60n * 60n,
+            },
+        );
+
+        // vote
+        const voter = await blockchain.treasury('voter');
+        const proposal = blockchain.openContract(
+            await Proposal.fromInit({
+                $$type: 'ProposalInit',
+                master: proposalMaster.address,
+                proposalId: 0n,
+            }),
+        );
+        await proposal.send(
+            voter.getSender(),
+            { value: toNano('0.1') },
+            {
+                $$type: 'Vote',
+                value: true,
+            },
+        );
+
+        // the vote was counted
+        expect(await proposal.getProposalState()).toMatchObject({ yesCount: 1n, noCount: 0n });
+    });
+
 });
